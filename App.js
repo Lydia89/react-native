1fd09ee9679a1bcc6df998aca8d1d56832943444
Lydia89 <lydy@live.fr>
2020-10-26 16:56:53 +0100

    exercicejour1

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, ScrollView, Image, Button, Alert } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'green',
+    // alignItems: 'center',
+    //justifyContent: 'center',
   },
 });
+
+
+
+class App extends Component {
+
+  render() {
+
+
+    return (
+      <ScrollView style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        {/** <Image source={require('./assets/konexio.png')}/> */}
+
+
+        <Image
+          source={{ uri: 'https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo_1.png' }}
+          style={{ width: 350, height: 100 }} />
+
+        <Button
+          onPress={() => {
+            alert('You tapped the button!');
+          }}
+          title="Press Me"
+        />
+        <StatusBar style="auto" />
+        <View>
+          < Text style={{ fontSize: 30, paddingTop: 80, paddingBottom: 80 }}> Konexio-reactNative</Text>
+          < Text style={{ textAlign: "center", paddingTop: 80, paddingBottom: 80 }}>reactNative</Text>
+          < Text style={{ fontWeight: 'bold', paddingTop: 80, paddingBottom: 80 }}>reactNative</Text>
+          <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+
+
+
+        </View>
+      </ScrollView>
+
+    );
+  }
+
+}
+
+
+
+export default App;
